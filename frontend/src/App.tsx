faeaf59d1570358665dc22b2e27d46ea9ddf3feb
unknown <sachitsinghbais777@gmail.com>
2023-05-12 11:22:00 +0530

    fw23_0339

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Login } from "./pages/Login";
 import { Home } from "./pages/home";
 
 function App() {
   return (
     <div>
       <Home />
+    
     </div>
   );
 }
