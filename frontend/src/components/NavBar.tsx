0efeba278e0e8dcafcf2e0c9c23a6418d27db919
Prabhat Kumar <prabhssgg3@gmail.com>
2023-05-12 18:45:32 +0530

    goalTracking

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a  className="flex items-center">
+        <a href="/"  className="flex items-center">
           <img src={logo} className="h-8 mr-3" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             FitQuestSquad
--- frontend/src/routes/Allroutes.tsx
+++ frontend/src/routes/Allroutes.tsx
@@ -3,14 +3,16 @@ import { Routes } from "react-router-dom";
 import { MealsExclusion } from "../components/meals/MealsExclusion";
 import { Home } from "../pages/home";
 import { Meals } from "../pages/Meals";
+import GoalTracking from "../pages/goalTracking";
 
 export const AllRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/meals" element={<Meals />} />
-        <Route path= "/meals/exclusions" element = {<MealsExclusion/>}/>
+        <Route path="/meals/exclusions" element={<MealsExclusion />} />
+        <Route path="/goaltracking" element={<GoalTracking />} />
       </Routes>
     </>
   );
